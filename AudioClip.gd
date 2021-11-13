6d8c738de58dcef0057f5df0c401af1d6b14e1bc
tangentstorm <michal.wallace@gmail.com>
2021-11-13 13:58:13 -0500

    draw waveform for audio clip

---
+++ AudioClip.gd
@@ -0,0 +1,72 @@
+extends ColorRect
+
+# draw audio clips
+# samples are in pcm format
+# pcm = pulse code modulation.
+# pulse code = amplitude as 8 or 16 bit number
+# data format is array of alternating (left sample, right sample) values
+
+export var sample : AudioStreamSample = null setget _set_sample
+export var timeScale : int = 128
+var bytesPerSample: int
+
+var offSet = 0
+
+func _set_sample(x):
+	sample = x
+	offSet = 0
+	bytesPerSample = _bytesPerSample()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	update()
+
+func _bytesPerSample():
+	var bytes = [1,2,0][sample.format]
+	if sample.stereo: bytes <<= 1
+	return bytes
+
+func sampleFormat():
+	var f = ["FORMAT_8_BITS", "FORMAT_16_BITS", "FORMAT_IMA_ADPCM"]
+	return f[sample.format]
+
+func printSampleInfo():
+	print("format:", sampleFormat())
+	print("mix rate:", sample.mix_rate)
+	print("stereo?:", sample.stereo)
+	print("length (seconds):", sample.get_length())
+	print("size(bytes):", sample.data.size())
+	print("bytes per sample:", bytesPerSample)
+	var s = sample
+	var t = (1.0*s.data.size()) / (bytesPerSample*s.mix_rate)
+	assert(abs(s.get_length() - t) < 0.000001)
+
+func getStereoSample16(i):
+	# help from: https://godotengine.org/qa/67091/how-to-read-audio-samples-as-1-1-floats
+	var d = sample.data; var j =  i + 2
+	var a = (((d[i] | (d[i+1]<<8)) + 32768)  & 0xffff) - 32768
+	return a # draw left channel only
+	# var b = (((d[j] | (d[j+1]<<8)) + 32768)  & 0xffff) - 32768
+	# return (a+b)>>1
+
+func _process(_dt):
+	pass
+	#if sample and t < sample.data.size():
+	#	update()
+
+func _draw():	
+	if sample:
+		printSampleInfo()
+		var o = Vector2(0,rect_size.y/2)
+		var pen = o + Vector2.ZERO
+		var vs = ""
+		var w = rect_size.x / sample.get_length()
+		for x in range(rect_size.x):
+			var i = timeScale*(offSet+x) * bytesPerSample
+			if i >= sample.data.size(): break
+			var v = getStereoSample16(i)/32768.0 * (rect_size.y /2)
+			vs += " " + str(v)
+			var next = o + Vector2(x, v)
+			draw_line(pen, next, Color.black)
+			pen = next
+		# offSet += rect_size.x / 2  # auto-scroll
--- main.gd
+++ main.gd
@@ -3,32 +3,23 @@ extends Node2D
 # project settings > audio > enable audio input
 
 var mic
-var clip
+var clip : AudioStreamSample
 
 func _ready():
 	var idx = AudioServer.get_bus_index("Record")
-	print("idx: ", idx)
 	mic = AudioServer.get_bus_effect(idx,0)
-	print(mic)
-	
 
 func _process(_dt):
 	if Input.is_action_just_pressed("ui_select"):
 		mic.set_recording_active(true)
-		print("recording!")
+
 	if Input.is_action_just_released("ui_select"):
-		print("recording active?", mic.is_recording_active())
 		clip = mic.get_recording()
 		mic.set_recording_active(false)
-		print("done")
-		print(clip.get_length())
-		print(clip.data.size())
-		var s:String = ""
-		for i in range(0,100):
-			s += str(clip.data[i])+" "
-		print(s)
-		
+
+		$AudioClip.sample = clip
+		$AudioClip.update()
+
 		$AudioStreamPlayer.stream = clip
 		$AudioStreamPlayer.play()
 		yield($AudioStreamPlayer, "finished")
-		

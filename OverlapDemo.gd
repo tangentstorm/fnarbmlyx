7fed6ebe0b0b8b6e522b59bb941aa9d278156179
tangentstorm <michal.wallace@gmail.com>
2021-11-19 02:54:32 -0500

    overlap demo

---
+++ OverlapDemo.gd
@@ -0,0 +1,67 @@
+# Overlap Demo ported to Godot
+# (the chunks of dead code here are meant to be stepping stones for
+# narrative presentation if I decide to record a video of this.)
+extends ColorRect
+
+var subject : Control = null
+var offset  : Vector2 = Vector2.ZERO
+var boxes   : Array
+
+func colorize_overlaps(pressed=false):
+	# brute force method - O(n^2)
+	# quadtree might be more appropriate with large numbers of boxes
+	for c in boxes: c.color = Color.white
+	for c0 in boxes:
+		var r0 = c0.get_rect()
+		for c1 in boxes:
+			if c1==c0: break
+			if r0.intersects(c1.get_rect()):
+				c0.color = Color.dimgray
+				c1.color = Color.dimgray
+	if pressed:
+		subject.color = (Color.goldenrod if subject.color == Color.white
+			else Color.black)
+
+func _input(e):
+	if e is InputEventMouseMotion:
+		$mouseXY.text = str(e.position)
+		if subject and e.button_mask:
+			subject.rect_position = e.position + offset
+#			var r = subject.get_rect()
+#			for c in get_children():
+#				if c is ColorRect and c != subject:
+#					if c.get_rect().intersects(r): c.color = Color.gray
+#					else: c.color = Color.white
+			colorize_overlaps(1==(e.button_mask&1))
+
+	if e is InputEventMouseButton:
+		if subject:
+			#subject.color = Color.goldenrod if e.pressed else Color.gold
+			colorize_overlaps(e.pressed)
+			if e.pressed:
+				offset = subject.rect_position - e.position
+				move_child(subject,get_child_count())
+#		for c in get_children():
+#			if c is ColorRect:
+#				if c.get_rect().has_point(e.position): c.color = Color.goldenrod
+#				else: c.color = Color.white
+
+func _ready():
+	for y in range(0, 3):
+		for x in range(0, 3):
+			var o = ColorRect.new()
+			o.rect_size = Vector2(32, 32)
+			o.rect_position = Vector2(50 + 75 * x, 50 + 75 * y)
+			add_child(o)
+			o.connect("mouse_entered", self, "on_mouse_enter", [o])
+			o.connect("mouse_exited",  self, "on_mouse_leave") #, [o])
+			boxes.append(o)
+
+func on_mouse_enter(x):
+	subject = x
+	x.color = Color.cornflower
+
+func on_mouse_leave():
+	subject = null
+	# x.color = Color.white
+	colorize_overlaps()

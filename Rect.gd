81e0f74a8674fe50bdffa7f66ead29bb0cf2cff5
tangentstorm <michal.wallace@gmail.com>
2021-11-27 12:51:39 -0500

    revive Rect.gd as plain rectangle with border.

--- Rect.gd
+++ Rect.gd
@@ -1,16 +1,10 @@
-tool extends ColorRect
+tool class_name Rect extends ColorRect
 
-export var strokeColor : Color = Color.black
-export var strokeWidth  : float = 1.0
+export var line_color : Color = Color.black
+export var line_width  : float = 1.0
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	update()
 
 func _draw():
-	draw_rect(Rect2(Vector2.ZERO, rect_size), strokeColor, false, strokeWidth)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	draw_rect(Rect2(Vector2.ZERO, rect_size), line_color, false, line_width)

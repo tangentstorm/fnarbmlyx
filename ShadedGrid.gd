272b37d1c18d4798bf872d8d908aa34ed8a587c8
tangentstorm <michal.wallace@gmail.com>
2021-11-27 12:52:52 -0500

    ShadedGrid: parameterize cell_size and rng_seed

--- ShadedGrid.gd
+++ ShadedGrid.gd
@@ -1,12 +1,19 @@
 tool extends ColorRect
 
-const cellSize = Vector2(32, 32)
+export var cell_size : Vector2 = Vector2(32, 32)
+export var rng_seed : int = 82076 setget set_seed
+var rng = RandomNumberGenerator.new()
+
+func set_seed(v):
+	rng_seed = v
+	rng.seed = v
+	update()
 
 func _draw():
 	var xy = Vector2.ZERO
 	while xy.y < rect_size.y:
 		xy.x = 0
 		while xy.x < rect_size.x:
-			draw_rect(Rect2(xy, cellSize), Color(int(rand_range(0,64))))
-			xy.x += cellSize.x
-		xy.y += cellSize.y
+			draw_rect(Rect2(xy, cell_size), Color(rng.randi_range(0,64)))
+			xy.x += cell_size.x
+		xy.y += cell_size.y

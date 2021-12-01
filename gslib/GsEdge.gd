ae58f514b978ff93d238c80ad9186951405844b8
tangentstorm <michal.wallace@gmail.com>
2021-11-30 19:34:40 -0500

    GsEdge: trap errors when src/dst path == ''

--- gslib/GsEdge.gd
+++ gslib/GsEdge.gd
@@ -8,14 +8,16 @@ func get_class_name(): return 'GsEdge'
 export var src_path : NodePath setget _set_src_path
 export var dst_path : NodePath setget _set_dst_path
 
-onready var src = get_node(src_path) setget _set_src
-onready var dst = get_node(dst_path) setget _set_dst
+onready var src = null if src_path == '' else get_node(src_path) setget _set_src
+onready var dst = null if dst_path == '' else get_node(dst_path) setget _set_dst
 
 func _set_src_path(v):
-	src_path = v; src = get_node(v)
+	src_path = v
+	if v != '': src = get_node(v)
 
 func _set_dst_path(v):
-	dst_path = v; dst = get_node(v)
+	dst_path = v;
+	if v != '': dst = get_node(v)
 
 func _set_src(v):
 	src = v
